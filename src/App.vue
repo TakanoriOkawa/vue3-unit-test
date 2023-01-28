ee7936e2b8aa62b19c485d977c5e449618748832
Taka <okawatakanori9916@gmail.com>
2023-01-29 01:12:17 +0900

    refとreactiveを使ったステート管理

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <script setup>
 import HelloWorld from './components/HelloWorld.vue'
+import SecondHelloWorld from './components/SecondHelloWorld.vue'
 </script>
 
 <template>
@@ -11,7 +12,8 @@ import HelloWorld from './components/HelloWorld.vue'
       <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
     </a>
   </div>
-  <HelloWorld msg="Vite + Vue" />
+  <HelloWorld msg="Vite + Vue" />  
+  <SecondHelloWorld />
 </template>
 
 <style scoped>
---
+++ src/components/SecondHelloWorld.vue
@@ -0,0 +1,25 @@
+<script setup>
+import { ref, reactive } from "vue";
+
+const num = ref(0);
+
+const state = reactive({
+  count: 1,
+});
+
+const numUp = () => {
+  num.value++;
+};
+
+const stateUp = () => {
+  state.count++;
+}
+</script>
+
+<template>
+  <h2>初めてのコンポーネント</h2>
+  <p>Ref Count: {{ num }}</p>
+  <p>Reactive Count: {{ state.count }}</p>
+  <button @click="numUp">カウントアップボタン</button>
+  <button @click="stateUp">ステート管理アップボタン</button>
+</template>
